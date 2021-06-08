f3635e62a8b88cd39b370cea2d6ed81383bd098a
Krulaks <matis.patryk@gmail.com>
2021-06-08 15:51:37 +0200

    Added basic messaging functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const socket = require("socket.io");
 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
@@ -10,3 +11,14 @@ const server = app.listen(PORT, () => {
 
 app.use(express.static("frontend")); //Jako fullstack użyłbym oczywiście React.js zamiast tego
 
+const io = socket(server);
+
+io.on("connection", socket => {
+    socket.on("message", msg =>{
+        socket.broadcast.emit("message", msg);
+    });
+
+    socket.on("typing", user => {
+        socket.broadcast.emit("typing", user); 
+    });
+})
---
+++ frontend/index.js
@@ -0,0 +1,63 @@
+const socket = io();
+const text = document.querySelector('#textarea');
+const typingMsg = document.querySelector('.typing-msg');
+const messages = document.querySelector('.messages');
+
+let username;
+while(!username){
+    username = prompt('What\'s your name');
+}
+
+text.addEventListener('keyup', event => {
+    if (event.key === 'Enter'){
+        sendMessage(event.target.value);
+    }
+
+    socket.emit('typing', {isTyping: text.value.length > 0, name: username});
+});
+
+
+const sendMessage = (msg => {
+    msg = msg.trim();
+    if (msg.length > 1){
+        const messageInfo = {
+            user: username,
+            message: msg
+        };
+    
+        socket.emit('message', messageInfo);
+        positionMessage(messageInfo, 'outgoing');
+    }
+    else {
+        alert("Your message must contain at least 2 characters.");
+    }
+    text.value = '';
+});
+
+
+const positionMessage = ((msg, type) => {
+    const div = document.createElement('div');
+    div.classList.add('messages--'+type);
+
+    const newElem = `
+        <h3>${msg.user}</h3>
+        <p>${msg.message}</p>
+    `;
+
+    div.innerHTML = newElem;
+    messages.appendChild(div);
+    messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+});
+
+
+socket.on('message', msg => {
+    positionMessage(msg, 'incoming');
+});
+
+socket.on('typing', user => {
+    if (user.isTyping){
+        typingMsg.innerHTML = `${user.name} is typing...`;
+    } else{
+        typingMsg.innerHTML = "";
+    }
+});
